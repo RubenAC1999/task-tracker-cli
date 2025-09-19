ba2d677b98169cf23305671cc36e28a5aa231a11
RubenAgra <rubenagra99@gmail.com>
2025-09-19 14:27:30 +0200

    add tasks function added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TaskManager taskManager = new TaskManager();
         String command = args[0];
 
         switch(command) {
             case "add":
+                if (args.length == 0) {
+                    System.out.println("Invalid arguments.");
+                }
                 if (args.length != 2) {
-                    System.err.println("Invalid arguments. Usage: add <title> <status>");
+                    System.err.println("Invalid arguments. Usage: add <title>");
                 }
 
                 Task task = taskManager.addTask(args[1]);
                 System.out.println("Task added successfully: " + task);
+                taskManager.writeTasks();
         }
     }
 }
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,8 +1,22 @@
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManager {
-    private List<Task> taskList;
+    private final List<Task> taskList;
     private int lastId = 0;
+    private final Path FILE = Path.of("tasks.json");
+
+    public TaskManager() throws IOException {
+        taskList = new ArrayList<>();
+        if(!Files.exists(FILE)) {
+            Files.createFile(FILE);
+        }
+    }
 
     public List<Task> getTaskList() {
         return taskList;
@@ -12,6 +26,28 @@ public class TaskManager {
         Task newTask = new Task(++lastId, description);
         taskList.add(newTask);
         return newTask;
+    }
+
+    public void writeTasks() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder taskString = new StringBuilder("{");
+
+        for (Task task : taskList) {
+            taskString
+                    .append(System.lineSeparator()).append("\"").append("id ").append("\"").append(":").append("\"").append(task.getId()).append("\",")
+                    .append(System.lineSeparator()).append("\"").append("descrption ").append("\"").append(":").append("\"").append(task.getDescription()).append("\",")
+                    .append(System.lineSeparator()).append("\"").append("status ").append("\"").append(":").append("\"").append(task.getStatus()).append("\",")
+                    .append(System.lineSeparator()).append("\"").append("createdAt ").append("\"").append(":").append("\"").append(task.getCreatedAt()).append("\",")
+                    .append(System.lineSeparator()).append("\"").append("updatedAt ").append("\"").append(":").append("\"").append(task.getUpdatedAt()).append("\"")
+                    .append(System.lineSeparator()).append("}");
+
+            if(!task.equals(taskList.getLast())) {
+               taskString.append(",");
+            }
+
+        }
+
+        Files.writeString(FILE, taskString);
     }
 
 
